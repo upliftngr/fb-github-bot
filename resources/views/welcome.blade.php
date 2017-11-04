830e1adeb1dad7ddfe355f8a934cbe11fb3cf858
Chinaka Light <chinakalight@googlemail.com>
2017-11-04 10:47:00 +0000

    updated the display text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel - Facebook GitHub Integration
+                    Facebook - GitHub Integration
                 </div>
 
                 <div class="links">
