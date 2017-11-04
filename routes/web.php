5ea4b14492ff8c2242e008d140c7630ce9af9f21
Chinaka Light <chinakalight@googlemail.com>
2017-11-04 19:24:13 +0000

    Web hooks mockups basics

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,48 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/test', 'ManageGitHubController@connectToGitHub');
+
+Route::get('/fbwebhook/', function() {
+    return "Hooks up with facebook";
+});
+
+Route::get('/messagefrombot/', function() {
+    return "Hello Mind, This is Chat bot, Your Controller";
+});
+
+Route::get('/speaktobot/', function() {
+    return "Hello world, I am this chat bot mind";
+});
+
+/*
+* ROUTES
+* User authorization callback URL
+* http://fbmsgis2.eastus.cloudapp.azure.com/gconfirm
+* The full URL to redirect to after a user authorizes an installation.
+*/
+Route::get('/gconfirm/', function() {
+    return "A required confirmation going on here";
+});
+
+
+/*  
+*   Setup URL (optional)
+*
+*	http://fbmsgis2.eastus.cloudapp.azure.com/additionalsetupconfirm
+* 	A URL to redirect users to after they install your GitHub App if additional setup is required on your end.
+*/
+Route::get('/additionalsetupconfirm/', function() {
+    return "Additional Setup Confirmation Going On Here";
+});
+
+
+/* 
+*	Webhook URL
+*	http://fbmsgis2.eastus.cloudapp.azure.com/listenhook
+* 	Events will POST to this URL. Read our webhook documentation for more information.
+*   https://developer.github.com/webhooks/
+*/
+Route::get('/listenhook/', function() {
+    return "This is GitHub Listening Hook";
+});
+
