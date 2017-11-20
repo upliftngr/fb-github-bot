693e6440cade1d35b5383036c8f9b974c0a1d686
Chinaka Light <chinakalight@googlemail.com>
2017-11-20 04:41:01 +0100

    modified input to access index again 2

--- app/Http/Controllers/ManageMessengerController.php
+++ app/Http/Controllers/ManageMessengerController.php
@@ -87,17 +87,23 @@ class ManageMessengerController extends Controller
     {
       try{
            $payloads = null;
-           $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+           $senderId = $input[0]['messaging'][0]['sender']['id'];
+           $messageText = $input[0]['messaging'][0]['message']['text'];
+           $postback = $input[0]['messaging'][0]['postback'];
+           $loctitle = $input[0]['messaging'][0]['message']['attachments'][0]['title'];
+
+           /*$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
            $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
            $postback = $input['entry'][0]['messaging'][0]['postback'];
-           $loctitle = $input['entry'][0]['messaging'][0]['message']['attachments'][0]['title'];
+           $loctitle = $input['entry'][0]['messaging'][0]['message']['attachments'][0]['title'];*/
+
            if (!empty($postback)) {
             $payloads = $input['entry'][0]['messaging'][0]['postback']['payload'];
             return ['senderid' => $senderId, 'message' => $payloads];
            }
 
            if (!empty($loctitle)) {
-            $payloads = $input['entry'][0]['messaging'][0]['postback']       ['payload'];
+            $payloads = $input['entry'][0]['messaging'][0]['postback']['payload'];
             return ['senderid' => $senderId, 'message' => $messageText, 'location' => $loctitle];
            }
 
